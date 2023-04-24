70017d072e5b8be5389d61491b678b66a6b006e2
Israel Lopes <ilsrael0406@gmail.com>
2023-04-24 12:06:11 -0300

    algoritimo js, css, e estrutura html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import validator from './validator.js';
+ import validator from './validator.js';
 
-console.log(validator);
+let btncard = document.getElementById("btn");
+btncard.addEventListener('click', start);
+
+function start(){
+    let numC = document.getElementById("numCard").value;
+    let output = document.getElementById("numbercard")
+
+    let valid = validator.isValid(numC);
+    let cardBlock = validator.block(numC);
+    let band = validator.bandCard(numC);
+
+    if (numC === ""){
+        output.innerHTML= "<h4 style='color: red;'> Digite o Numero do Cartão</h4>"
+    }
+    else if(valid === true){
+        output.innerHTML= cardBlock+band+"<h4 style='color: green;'> Cartão Válido</h4>"
+    }
+    else{
+        output.innerHTML= cardBlock+ "<h4 style='color: red;'>Cartão Inválido</h4>"
+
+    }
+} 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,56 @@
 const validator = {
-  // ...
-};
+
+  isValid: function (card){
+    let Ncard = card.split("").reverse();
+    let rescard = 0;
+
+    for (let i =0; i<Ncard.length; i++){
+      let num1Card = parseInt(Ncard[i])
+
+      if(i%2 !== 0 && num1Card >= 5){
+        rescard = rescard + (num1Card*2)-9;
+      }
+      else if(i%2 !== 0 && num1Card < 5){
+        rescard = rescard + (num1Card*2);
+      }
+      else{rescard = rescard + num1Card;}
+  }
+  if (rescard%10 === 0){
+    return true;
+  }
+  else{
+    return false;
+  } 
+},
+
+block: function (card1){
+  return card1.replace(/\d(?=\d{4})/g,"#")
+},
+
+bandCard: function (bandNum){
+  var bandNum = bandNum.replace(/[^0-9]+/g, '');
+
+  var cards = {
+      visa      : /^4[0-9]{12}(?:[0-9]{3})/,
+      mastercard : /^5[1-5][0-9]{14}/,
+      diners    : /^3(?:0[0-5]|[68][0-9])[0-9]{11}/,
+      amex      : /^3[47][0-9]{13}/,
+      discover  : /^6(?:011|5[0-9]{2})[0-9]{12}/,
+      hipercard  : /^(606282\d{10}(\d{3})?)|(3841\d{15})/,
+      elo        : /^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})/,
+      jcb        : /^(?:2131|1800|35\d{3})\d{11}/,
+      aura      : /^(5078\d{2})(\d{2})(\d{11})$/
+  }
+
+  for (var i in cards) {
+      if(cards[i].test(bandNum)) {
+          return i;
+      }
+  }
+
+  return false;
+}
+
+}
 
 export default validator;
